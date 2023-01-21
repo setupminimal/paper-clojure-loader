73e774becef99582489b2bd1a827a3aa6c4e91d6
Daroc Alden <setupminimal@gmail.com>
2023-01-21 15:45:53 -0500

    Clojure-loading

--- src/main/java/io/github/setupminimal/treeplugin/TreePlugin.java
+++ src/main/java/io/github/setupminimal/treeplugin/TreePlugin.java
@@ -1,14 +1,32 @@
-package com.crimsonwarpedcraft.exampleplugin;
+package io.github.setupminimal.treeplugin;
 
+import clojure.lang.IFn;
 import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * A plugin class that loads clojure code.
+ */
 public class TreePlugin extends JavaPlugin {
+
+  private void reload() {
+    // Set the contextual classloader for clojure code
+    Thread.currentThread().setContextClassLoader(
+        this.getClass().getClassLoader());
+
+    IFn loadFile = Clojure.var("clojure.core", "load-file");
+    loadFile.invoke("main.clj");
+
+    IFn registerFn = Clojure.var("main", "register");
+    registerFn.invoke(this);
+  }
 
   @Override
   public void onEnable() {
     PaperLib.suggestPaper(this);
 
-    saveDefaultConfig();
+    reload();
+
+    getLogger().info("Tree plugin has been enabled");
   }
 }
