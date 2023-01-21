649b8dc86d60519be42f2ac726aeca20cc55569e
Daroc Alden <setupminimal@gmail.com>
2023-01-21 12:19:06 -0500

    Forking template

--- src/main/java/io/github/setupminimal/treeplugin/TreePlugin.java
+++ src/main/java/io/github/setupminimal/treeplugin/TreePlugin.java
@@ -3,13 +3,7 @@ package com.crimsonwarpedcraft.exampleplugin;
 import io.papermc.lib.PaperLib;
 import org.bukkit.plugin.java.JavaPlugin;
 
-
-/**
- * Created by Levi Muniz on 7/29/20.
- *
- * @author Copyright (c) Levi Muniz. All Rights Reserved.
- */
-public class ExamplePlugin extends JavaPlugin {
+public class TreePlugin extends JavaPlugin {
 
   @Override
   public void onEnable() {
